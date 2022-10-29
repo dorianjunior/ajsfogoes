34e59da49dbde62832dee352dac3d003f7fe6a0e
Dorian <dorianc.vieira@gmail.com>
2022-10-29 02:39:39 -0300

    Melhorias de SEO

---
+++ public/index.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>AJS Fogões | Conserto e Manutenção de Fogões e Cooktops</title>
+  <meta content="Conserto e manutenção em toda linha de fogões a gás (industriais e residenciais). Há 10 anos no ramo, com honestidade e preço justo. Atendimento em Florianópolis. Serviço com garantia." name="description">
+  <meta content="AJS Fogões Consertos Instalações Assistência Técnica Autorizada Brastemp Dinoxx Electrolux Fischer" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.ico" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center header-transparent">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <div class="logo">
+        <h1><a href="index.html">AJS Fogões</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Início</a></li>
+          <li><a class="nav-link scrollto" href="#about">Sobre</a></li>
+          <li><a class="nav-link scrollto" href="#services">Serviços</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Contato</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+      <!-- .navbar -->
+
+    </div>
+  </header>
+  <!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+      <h1>Instalação e Manutenção de Fogões</h1>
+      <h2>Residenciais e Industriais, Conversão GLP para GN e Tubulações de gás</h2>
+      <a href="#contact" class="btn-get-started scrollto">Agende uma avaliação!</a>
+    </div>
+  </section>
+  <!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="row content">
+          <div class="col-lg-6">
+            <h2>Sua busca por serviços de qualidade acabou!</h2>
+            <h3>Asseguramos que a nossa principal intenção é prestar o melhor serviço possível pra que todos fiquem satisfeitos.</h3>
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0">
+            <p>
+              Há mais de 10 anos de experiência no mercado, a AJS Fogões tem sempre o objetivo de atender seus clientes e amigos no segmento de:
+            </p>
+            <ul>
+              <li><i class="ri-check-double-line"></i> Conserto, manutenção de fogões domésticos e cooktops</li>
+              <li><i class="ri-check-double-line"></i> Instalação e manutenção de churrasqueiras a gás Dinoxx</li>
+              <li><i class="ri-check-double-line"></i> Consertos de fogões industriais – sob consulta</li>
+              <li><i class="ri-check-double-line"></i> Instalação de Tubulações de gás</li>
+            </ul>
+            <p class="fst-italic">
+              Oferecemos serviços regulares ou reparos de emergência para resolver o problema. Além de trabalharmos com diversas marcas e modelos, também fornecemos manutenção preventiva cuidadosa, serviço altamente recomendado para todos os seus equipamentos.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <!-- End About Section -->
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Serviços</h2>
+          <p>Atendemos de segunda a sexta, das 8h às 18h.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6 icon-box">
+            <div class="icon"><i class="bi bi-house-door"></i></div>
+            <h4 class="title"><a href="">Atendimento a domicílio</a></h4>
+            <p class="description">Oferecemos atendimento na sua empresa ou no conforto do seu lar</p>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box">
+            <div class="icon"><i class="bi bi-gear"></i></div>
+            <h4 class="title"><a href="">Manutenção e Instalação</a></h4>
+            <p class="description">Trabalhamos com as mais diversas marcas de cooktops, fornos, fogões domésticos, fogões embutidos, churrasqueiras a gás Dinoxx e fogões industriais</p>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box">
+            <div class="icon"><i class="bi bi-tools"></i></div>
+            <h4 class="title"><a href="">Conversão de gás</a></h4>
+            <p class="description">Se mudou ou comprou um fogão novo? Fazemos a conversão de GN para GPL e vice e versa</p>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box">
+            <div class="icon"><i class="bi bi-binoculars"></i></div>
+            <h4 class="title"><a href="">Tubulações de gás</a></h4>
+            <p class="description">Ofereceremos soluções para sistemas de gás canalizado em comércio e residência</p>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box">
+            <div class="icon"><i class="bi bi-thermometer-sun"></i></div>
+            <h4 class="title"><a href="">Regulagem das chamas</a></h4>
+            <p class="description">Seu fogão demora para cozinhar e ainda deixa as panelas sujas? Nós temos a solução</p>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box">
+            <div class="icon"><i class="bi bi-calendar4-week"></i></div>
+            <h4 class="title"><a href="">Revisão completa</a></h4>
+            <p class="description">Faça uma revisão completa para ter certeza que tudo está bem</p>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <!-- End Services Section -->
+
+    <!-- ======= Frequently Asked Questions Section ======= -->
+    <section id="faq" class="faq d-none">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Frequently Asked Questions</h2>
+        </div>
+
+        <div class="row  d-flex align-items-stretch">
+
+          <div class="col-lg-6 faq-item">
+            <i class="bx bx-help-circle"></i>
+            <h4>Non consectetur a erat nam at lectus urna duis?</h4>
+            <p>
+              Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
+            </p>
+          </div>
+
+          <div class="col-lg-6 faq-item">
+            <i class="bx bx-help-circle"></i>
+            <h4>Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque?</h4>
+            <p>
+              Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim.
+            </p>
+          </div>
+
+          <div class="col-lg-6 faq-item">
+            <i class="bx bx-help-circle"></i>
+            <h4>Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi?</h4>
+            <p>
+              Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus.
+            </p>
+          </div>
+
+          <div class="col-lg-6 faq-item">
+            <i class="bx bx-help-circle"></i>
+            <h4>Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla?</h4>
+            <p>
+              Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim.
+            </p>
+          </div>
+
+          <div class="col-lg-6 faq-item">
+            <i class="bx bx-help-circle"></i>
+            <h4>Tempus quam pellentesque nec nam aliquam sem et tortor consequat?</h4>
+            <p>
+              Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in
+            </p>
+          </div>
+
+          <div class="col-lg-6 faq-item">
+            <i class="bx bx-help-circle"></i>
+            <h4>Tortor vitae purus faucibus ornare. Varius vel pharetra vel turpis nunc eget lorem dolor?</h4>
+            <p>
+              Laoreet sit amet cursus sit amet dictum sit amet justo. Mauris vitae ultricies leo integer malesuada nunc vel. Tincidunt eget nullam non nisi est sit amet. Turpis nunc eget lorem dolor sed. Ut venenatis tellus in metus vulputate eu scelerisque.
+            </p>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+    <!-- End Frequently Asked Questions Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Contato</h2>
+          <p>Ficou alguma dúvida? Entre em contato conosco e vamos esclarecer todas as suas perguntas.</p>
+        </div>
+      </div>
+      <div class="map d-none">
+        <iframe style="border:0; width: 100%; height: 350px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3539.4942070677334!2d-48.41345638543157!3d-27.485001723933074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9527403cf057f52b%3A0x4576036ce2a07fc4!2sR.%20C%C3%A2ndido%20Pereira%20dos%20Anjos%20-%20S%C3%A3o%20Jo%C3%A3o%20do%20Rio%20Vermelho%2C%20Florian%C3%B3polis%20-%20SC%2C%2088060-300!5e0!3m2!1sen!2sbr!4v1654462231594!5m2!1sen!2sbr" frameborder="0" allowfullscreen></iframe>
+      </div>
+      <div class="container">
+        <div class="row mt-5 justify-content-center">
+
+          <div class="col-lg-10">
+
+            <div class="info-wrap">
+              <div class="row">
+                <div class="col-lg-4 info">
+                  <i class="bi bi-geo-alt"></i>
+                  <h4>Endereço:</h4>
+                  <p>R. Cândido Pereira dos Anjos <br> Florianópolis - SC, 88060-300</p>
+                </div>
+
+                <div class="col-lg-4 info mt-4 mt-lg-0 ">
+                  <i class="bi bi-envelope"></i>
+                  <h4>Email:</h4>
+                  <a href="mailto:ajsfogoes@bol.com.br?subject=Solicita%C3%A7%C3%A3o%20de%20Servi%C3%A7o&body=Ol%C3%A1%2C%20Preciso%20de%20seus%20servi%C3%A7os." target="_blank">
+                    <p>ajsfogoes@bol.com.br</p>
+                  </a>
+                </div>
+
+                <div class="col-lg-4 info mt-4 mt-lg-0">
+                  <a href="https://api.whatsapp.com/send/?phone=5548991572962&text=Ol%C3%A1%2C+preciso+de+seus+servi%C3%A7os.&app_absent=0" target="_blank"><i class="bi bi-whatsapp"></i></a>
+                  <h4>Telefone:</h4>
+                  <p>(48) 99157-2962 </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End Contact Section -->
+
+  </main>
+  <!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+
+      <div class="container">
+
+        <div class="row  justify-content-center">
+          <div class="col-lg-6">
+            <h3>AJS Fogões</h3>
+            <p>Serviço profissional e de qualidade.</p>
+          </div>
+        </div>
+
+        <div class="row footer-newsletter justify-content-center d-none">
+          <div class="col-lg-6">
+            <form action="" method="post">
+              <input type="email" name="email" placeholder="Enter your Email"><input type="submit" value="Subscribe">
+            </form>
+          </div>
+        </div>
+
+        <div class="social-links">
+          <a href="#" class="twitter d-none"><i class="bx bxl-twitter"></i></a>
+          <a href="https://www.facebook.com/AJS-Fog%C3%B5es-100567452552732/" target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="https://api.whatsapp.com/send/?phone=5548991572962&text=Ol%C3%A1%2C+preciso+de+seus+servi%C3%A7os.&app_absent=0" target="_blank" class="whatsapp"><i class="bx bxl-whatsapp"></i></a>
+          <a href="https://www.instagram.com/ajsf_ogoes/" target="_blank" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="mailto:ajsfogoes@bol.com.br?subject=Solicita%C3%A7%C3%A3o%20de%20Servi%C3%A7o&body=Ol%C3%A1%2C%20Preciso%20de%20seus%20servi%C3%A7os." target="_blank" class="email"><i class="bx bx-mail-send"></i></a>
+          <a href="#" class="google-plus d-none"><i class="bx bxl-skype"></i></a>
+          <a href="#" class="linkedin d-none"><i class="bx bxl-linkedin"></i></a>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+        &copy; Copyright <strong><span>AJS Fogões</span></strong>. Todos os direitos reservados
+      </div>
+    </div>
+  </footer>
+  <!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
